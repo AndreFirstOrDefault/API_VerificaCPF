a916ded84df83c4088ccb84f9e7436de71005c29
AndreFirstOrDefault <andre.java.ads.2022@gmail.com>
2024-03-04 20:23:08 -0300

    Criado método de validacao automatica de CPF com ou sem mascara

--- VerificaCPF/Controllers/ValidaCPFController.cs
+++ VerificaCPF/Controllers/ValidaCPFController.cs
@@ -80,4 +80,54 @@ public class ValidaCPFController : ControllerBase
         var cpfMascara = FormataCPFServices.FormataCPFMascara(cpf);
         return Ok($"CPF gerado: {cpfMascara}");
     }
+
+    [HttpGet("ValidarCPF xxxxxxxxxxx ou xxx.xxx.xxx-xx ")]
+    public IActionResult ValidarCPFAutomatico(string cpf)
+    {
+        if (ContadorDeCaracteresServices.ContadorDeCaracteres(cpf, 11))
+        {
+            var validator2 = new VerificaCaracteresAttribute();
+            var result2 = validator2.IsValid(cpf);
+
+            if (result2 == false)
+            {
+                return BadRequest("O CPF deve conter somente numeros!");
+            }
+
+            var validator = new VerificaDigitosAttribute();
+            var result = validator.IsValid(cpf);
+
+            if (result)
+            {
+                return Ok("CPF válido.");
+            }
+
+            return BadRequest("CPF inválido.");
+        }
+
+        if (ContadorDeCaracteresServices.ContadorDeCaracteres(cpf, 14))
+        {
+            var validator = new VerificaCaracteresAttribute();
+            var result = validator.IsValid(cpf);
+
+            if (result == false)
+            {
+                return BadRequest("O CPF não deve conter letras!");
+            }
+
+            var cpfFormatado = FormataCPFServices.FormataCPF(cpf);
+
+            var validator2 = new VerificaDigitosAttribute();
+            var result2 = validator2.IsValid(cpfFormatado);
+
+            if (result2)
+            {
+                return Ok("CPF válido");
+            }
+
+            return BadRequest("CPF inválido");
+        }
+
+        return BadRequest("Ocorreu um erro desconhecido!");
+    }
 }
