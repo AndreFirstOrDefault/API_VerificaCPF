b755dee215326b8429c2b2b688af3c51956f4e29
AndreFirstOrDefault <andre.java.ads.2022@gmail.com>
2024-03-08 18:49:51 -0300

    Adaptação do projeto para subir no Cloud Run do GCP

--- VerificaCPF/Program.cs
+++ VerificaCPF/Program.cs
@@ -2,18 +2,46 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+// Configure the app
+builder.Host.ConfigureWebHostDefaults(webBuilder =>
+{
+    var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+    var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")??"Development";
+
+    if(environment == "Development")
+    {
+        webBuilder.UseStartup<StartupBase>();
+    }
+    else
+    {
+        var url = $"http://0.0.0.0:{port}";
+        webBuilder.UseStartup<StartupBase>().UseUrls(url);
+    }
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
